ababfe582d4f74e72e03685092f1fd18ab223a73
Aleksejs Pjavka <apjavka07@gmail.com>
2022-05-21 17:44:49 +0300

    vidget design added

--- screen/menu_main.go
+++ screen/menu_main.go
@@ -2,6 +2,7 @@ package screen
 
 import (
 	"eklase/state"
+	"image/color"
 
 	"gioui.org/layout"
 	"gioui.org/widget"
@@ -16,10 +17,20 @@ func MainMenu(th *material.Theme, state *state.State) Screen {
 		quit widget.Clickable
 	)
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
+		matAddBut := material.Button(th, &add, "Add student")
+		matAddBut.Font = text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Alegreya}
+		matAddBut.Background = color.NRGBA{A: 0xff, R: 0xb4, G: 0xb4, B: 0xb4}
+		matListBut := material.Button(th, &list, "List students")
+		matListBut.Font = text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Alegreya}
+		matListBut.Background = color.NRGBA{A: 0xff, R: 0xb4, G: 0xb4, B: 0xb4}
+		matQuitBut := material.Button(th, &quit, "Quit")
+		matQuitBut.Font = text.Font{Variant: "Smallcaps", Style: text.Alegreya}
+		matQuitBut.Background = color.NRGBA{A: 0xff, R: 0xb4, G: 0xb4, B: 0xb4}
+
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-			layout.Rigid(rowInset(material.Button(th, &add, "Add student").Layout)),
-			layout.Rigid(rowInset(material.Button(th, &list, "List students").Layout)),
-			layout.Rigid(rowInset(material.Button(th, &quit, "Quit").Layout)),
+			layout.Rigid(rowInset(matAddBut.Layout)),
+			layout.Rigid(rowInset(matListBut.Layout)),
+			layout.Rigid(rowInset(matQuitBut.Layout)),
 		)
 		if add.Clicked() {
 			return AddStudent(th, state), d
