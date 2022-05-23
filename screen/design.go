de0ca6fe19cf2964bf411fa20f1f9adc873ac483
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-05-23 22:20:50 +0300

    all design parametres now here

---
+++ screen/design.go
@@ -0,0 +1,14 @@
+package screen
+
+import (
+	"image/color"
+
+	"gioui.org/text"
+)
+
+func ButtonFontMain() (Font text.Font) {
+	return text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Italic}
+}
+func ButtonBacgGroundMain() (Background color.NRGBA) {
+	return color.NRGBA{A: 0xff, R: 0x3C, G: 0x3C, B: 0x3C}
+}
